947263915839c5a7cf19331f55c6a4eadcae807e
Joseph Mead <josephmmead@gmail.com>
2020-11-09 21:22:03 -0500

    fixed hidden error bug

--- db/store.js
+++ db/store.js
@@ -1,64 +1,50 @@
 const util = require("util");
 const fs = require("fs");
-​
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const { v4: uuidv4 } = require('uuid');
-​
+const { uuid } = require('uuidv4');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-​
 class Store {
-
   read() {
-    return readFileAsync("db/db.json", "utf8");
-  };
-​
+    return readFileAsync("db/db.json", "utf8")
+  }
+
   write(note) {
-    return writeFileAsync("db/db.json", JSON.stringify(note));
-  };
-​
+    return writeFileAsync("db/db.json", JSON.stringify(note))
+  }
+
   getNotes() {
-    return this.read().then((notes) => {
-       
+    return this.read().then((notes) => {     
       let parsedNotes;
-​
       // If notes isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedNotes = [].concat(JSON.parse(notes))
       } catch (err) {
         parsedNotes = []
       }
-​
-      return parsedNotes;
-    });
-    
-  };
-​
+      return parsedNotes
+    })
+  }
+
   addNotes(note) {
-    // set up variables with our notes data here
-     
-​     const {title, text, } = note;
-​     
+    // set up variables with our notes data here 
+    const {title, text} = note
     // Error handle here, if we have no title or text added throw a new error explaining what is wrong
     if (!title || !text) {
       throw new Error("Note 'title' and 'text' cannot be blank")
     }
-​
-    const newNote = { title, text, id: uuidv4() };
-
+    const newNote = {title, text, id: uuid()}
     // console.log(newNote);
     // Add a unique id to the note using uuid package
-​     return this
+    return this
      .getNotes().then((notesArray) => [...notesArray, newNote]).then((newNotes) => this.write(newNotes)).then(() => newNote)
     // Get all notes, add the new note, write all the updated notes, return the newNote
-  
-  };
-​
+  }
+
   removeNotes(id) {
     // Get all notes, remove the note with the given id, write the filtered notes
     return this
     .getNotes().then((notes) => notes.filter((note) => note.id !== id)).then((updatedNotes) => this.write(updatedNotes))
   }
 };
-​
-module.exports = new Store();
+module.exports = new Store()
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const Store = require("../db/store");
+const store = require("../db/store.js");
 
 
 router.get("/notes", (req, res) => {
     // this is the GET route where you 
     // will utilize the getNotes() function
-    Store.getNotes().then((notes) => res.json(notes)).catch((err) => res.status(500).json(err))
+    store.getNotes().then((notes) => res.json(notes)).catch((err) => res.status(500).json(err))
     
     
 });
@@ -13,14 +13,14 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     // this is the POST route where you 
     // will utilize the addNotes() function
-    Store.addNotes(req.body).then((notes) => res.json(notes)).catch((err) => res.status(500).json(err))
+    store.addNotes(req.body).then((notes) => res.json(notes)).catch((err) => res.status(500).json(err))
       
 });
 
 router.delete("/notes/:id", (req, res) => {
     // this is the delete route where you will
     // utilize the removeNote() function
-    Store.removeNotes(req.params.id).then(() => res.json({ ok: true })).catch((err) => res.status(500).json(err))
+    store.removeNotes(req.params.id).then(() => res.json({ ok: true })).catch((err) => res.status(500).json(err))
 
 });
 
